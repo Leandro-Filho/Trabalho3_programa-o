ebd30c9e0a01e3a288aceeb8c9636b19ac778ac1
bebaran <leandro.filho@sou.inteli.edu.br>
2025-09-03 18:01:26 -0300

    feat: feita a metade da ponderada

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -18,25 +18,38 @@ def classificar_acima_do_peso(imc):
 df["overweight"] = df["IMC"].apply(classificar_acima_do_peso)
 
 # 3
-
+# cholesterol e gluc
+def transformar_valores(valores):
+    if valores == 1:
+        return 0
+    else:
+        if valores > 1:
+            return 1
+        
+df['cholesterol'] = df['cholesterol'].apply(transformar_valores)
+df['gluc'] = df['gluc'].apply(transformar_valores)
 
 # 4
-def draw_cat_plot():
-    # 5
-    df_cat = None
-
-
+def draw_cat_plot(df):
+    #5
+    df_cat = pd.melt(df,
+                     value_vars=['gluc', 'cholesterol', 'smoke', 'alco', 'active', 'cardio'])
     # 6
-    df_cat = None
-    
-
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
     # 7
-
-
-
-    # 8
-    fig = None
-
+         # 8
+    fig = sns.catplot(
+        x='variable',
+        y='total',
+        hue='value',
+        col='cardio',
+        data=df_cat,
+        kind='bar',
+        height=5,
+        aspect=1
+    )
+    fig.set_axis_labels("Categoria", "Contagem")
+    fig.set_titles("Cardio = {col_name}")
 
     # 9
     fig.savefig('catplot.png')
